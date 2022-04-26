50c0ce04bfa2b0fc683e271feb271773c56686cd
wserr <serruys.willem@hotmail.com>
2022-04-26 21:26:58 +0200

    build script backend

---
+++ src/backend/scripts/build-and-push-local-cluster.sh
@@ -0,0 +1,3 @@
+docker build . -t localhost:32000/backend:latest
+
+docker push localhost:32000/backend:latest
--- src/frontend/scripts/build-and-push-local-cluster.sh
+++ src/frontend/scripts/build-and-push-local-cluster.sh
@@ -1,3 +1,3 @@
-sudo docker build . -t localhost:32000/frontend:latest --build-arg BUILD_CMD=build-local-cluster
+docker build . -t localhost:32000/frontend:latest --build-arg BUILD_CMD=build-local-cluster
 
-sudo docker push localhost:32000/frontend:latest
+docker push localhost:32000/frontend:latest
